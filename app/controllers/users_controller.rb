9263d29da9ea685fef4d9aeed61e8097134245d2
Wai Ian Vong <valenvwi@gmail.com>
2023-06-11 10:07:01 +0200

    Add edit page with view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update ]
 
   def index
     @users = User.all
@@ -12,6 +13,24 @@ class UsersController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: "user was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar, :username, :bio, :location)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  validates :first_name, :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :bio, length: { maximum: 50 }
 end
