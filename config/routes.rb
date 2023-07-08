7f12e2a0cbf5202ba1c3a69ed4db0028ba032995
Wai Ian Vong <valenvwi@gmail.com>
2023-07-08 13:16:21 +0200

    username in comment link to user profile, and post show css

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :follows, only: [:create, :destroy]
   end
   resources :posts do
-    resources :likes
-    resources :comments
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
 end
