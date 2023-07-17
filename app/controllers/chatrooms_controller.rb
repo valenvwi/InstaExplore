0565f89d1ae06410f589d87b8d3ad41a3406f9b6
Wai Ian Vong <valenvwi@gmail.com>
2023-07-17 22:31:49 +0200

    add chatroom and message without redis, add chatroom index and show, and _message

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,23 @@
+class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = policy_scope(Chatroom).where("owner_id = ? OR guest_id = ?", current_user.id, current_user.id)
+    authorize @chatrooms
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+    @message = Message.new
+    authorize @chatroom
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @chatroom = Chatroom.new
+    @chatroom.owner = current_user
+    @chatroom.guest = @user
+    @chatroom.save
+    redirect_to chatroom_path(@chatroom)
+    authorize @user
+  end
+
+end
