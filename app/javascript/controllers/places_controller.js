72e6f7467fbb2214f571a937a94b234161346eaa
Wai Ian Vong <valenvwi@gmail.com>
2023-09-21 09:18:54 +0200

    fix footer

--- app/javascript/controllers/places_controller.js
+++ app/javascript/controllers/places_controller.js
@@ -1,12 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
-
 // Connects to data-controller="places"
 export default class extends Controller {
   static values = { apiKey: String };
 
   static targets = ["input"];
 
-  // this is to make sure that the google api is not loaded twice
   googleApiInitialized = false;
 
   connect() {
@@ -18,16 +16,13 @@ export default class extends Controller {
     if (this.googleApiInitialized) return;
     const script = document.createElement("script");
     script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKeyValue}&libraries=places&callback=initMap`;
-    console.log(script.src);
 
-    // if the script is loading, it doesn't stop the whole page to load
     script.async = true;
 
     window.initMap = () => {
       this.initializeAutocomplete();
     };
 
-    // this is to make sure that the script is loaded in the head of the html
     document.head.appendChild(script);
 
     this.googleApiInitialized = true;
@@ -40,11 +35,7 @@ export default class extends Controller {
     };
 
     new google.maps.places.Autocomplete(input, options);
-    // const autocomplete = new google.maps.places.Autocomplete(input, options);
-    // autocomplete.addListener('place_changed', () => {
-    //   const place = autocomplete.getPlace();
-    //   console.log(place);
-    // });
+
   }
 }
 
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,3 +2,5 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 end
+
+# https://github.com/excid3/noticed
